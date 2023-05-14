b58b6c1112c83e1d199792cc7b60796524836f17
wdaherahmed <wdaherahmed@deloitte.com>
2023-05-14 17:03:38 +0200

    Recipe+Tag Model and API

--- app/app/settings.py
+++ app/app/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'drf_spectacular',
     'core',
     'user',
+    'recipe',
+
 ]
 
 MIDDLEWARE = [
---
+++ app/recipe/views.py
@@ -0,0 +1,49 @@
+"""
+Views for the recipe APIs
+"""
+from rest_framework import (
+    viewsets,
+    mixins,
+)
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from core.models import (
+    Recipe,
+    Tag,
+)
+from recipe import serializers
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """View for manage recipe APIs."""
+    serializer_class = serializers.RecipeDetailSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Retrieve recipes for authenticated user."""
+        return self.queryset.filter(user=self.request.user).order_by('-id')
+
+    def get_serializer_class(self):
+        """Return the serializer class for request."""
+        serializer = self.serializer_class
+        if self.action == 'list':
+            serializer = serializers.RecipeSerializer
+        return serializer
+    
+    def perform_create(self, serializer):
+        """Create a new recipe."""
+        serializer.save(user=self.request.user)
+
+class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    """Manage tags in the database."""
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Filter queryset to authenticated user."""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
